e4fbd1874fe0ff7d3bf6379c142d5bb2179fabf5
George Fedoseev <george.fedoseev@me.com>
2018-06-27 12:18:23 +0300

    working timestamp

---
+++ functions/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+app.get("/timestamp", (req, resp) => {
+    resp.send(`${Date.now()}`);
+});
+exports.app = functions.https.onRequest(app);
+//# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+app.get("/timestamp", (req, resp) => {
+    resp.send(`${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app)
