048577f961c69bc56b092e13cb4afb34562f6d2f
George Fedoseev <george.fedoseev@me.com>
2018-07-01 10:40:46 +0300

    changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,15 +4,17 @@ import * as functions from 'firebase-functions';
 import * as express from 'express'
 import * as engines from 'consolidate'
 
+import * as path from 'path'
+
 
 const app = express();
-app.engine('hbs', engines.handlebars);
+app.engine('html', engines.handlebars);
 app.set('views', './src/views');
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
 
 
 app.get("/test", (request, response) => {
-    response.render('index', {number: 42});
+    response.render('index', {number: 45});    
 });
 
 exports.app = functions.https.onRequest(app)
